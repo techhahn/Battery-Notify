7b002fc9f4659f6063cc36bd552371d3e7828500
Pritesh Panjiker <priteshpanjiker@gmail.com>
2013-12-16 10:44:25 +0530

    Added Project files to the repository

---
+++ battery notify/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace battery_notify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ChargeRemaining = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.batteryFull = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BatteryStatus = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ChargeRemaining
+            // 
+            this.ChargeRemaining.AutoSize = true;
+            this.ChargeRemaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChargeRemaining.Location = new System.Drawing.Point(310, 9);
+            this.ChargeRemaining.Name = "ChargeRemaining";
+            this.ChargeRemaining.Size = new System.Drawing.Size(109, 39);
+            this.ChargeRemaining.TabIndex = 0;
+            this.ChargeRemaining.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.batteryFull);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.BatteryStatus);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(15, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(226, 153);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // batteryFull
+            // 
+            this.batteryFull.AutoSize = true;
+            this.batteryFull.BackColor = System.Drawing.SystemColors.Control;
+            this.batteryFull.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.batteryFull.Location = new System.Drawing.Point(17, 53);
+            this.batteryFull.Name = "batteryFull";
+            this.batteryFull.Size = new System.Drawing.Size(0, 73);
+            this.batteryFull.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Status:";
+            // 
+            // BatteryStatus
+            // 
+            this.BatteryStatus.AutoSize = true;
+            this.BatteryStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BatteryStatus.Location = new System.Drawing.Point(89, 28);
+            this.BatteryStatus.Name = "BatteryStatus";
+            this.BatteryStatus.Size = new System.Drawing.Size(70, 25);
+            this.BatteryStatus.TabIndex = 0;
+            this.BatteryStatus.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Battery Percentae:";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.Text = "Battery Notify";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Gray;
+            this.linkLabel1.Location = new System.Drawing.Point(446, 217);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(133, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "priteshpanjiker@gmail.com";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(13, 124);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(165, 29);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Add to startup";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 239);
+            this.ControlBox = false;
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ChargeRemaining);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.7D;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Battery Notfy";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ChargeRemaining;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label BatteryStatus;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label batteryFull;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ battery notify/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace battery_notify
+{
+ 
+    public partial class Form1 : Form
+    {
+
+        PowerStatus power = SystemInformation.PowerStatus;
+
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            notifyIcon1.ShowBalloonTip(2000, "Battery Full", "Please Unplug the Charger", ToolTipIcon.Warning);
+            RefreshStatus();
+            timer1.Enabled = true;
+
+            float secondsRemaining = power.BatteryLifePercent;
+            if (secondsRemaining >= 0)
+            {
+                ChargeRemaining.Text = (secondsRemaining * 100).ToString() + "% available.";
+            }
+            else
+            {
+                ChargeRemaining.Text = string.Empty;
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            RefreshStatus();
+        }
+
+        private void RefreshStatus()
+        {
+            int powerPercent = (int)(power.BatteryLifePercent * 100);
+
+            switch (power.PowerLineStatus)
+            {
+
+                case PowerLineStatus.Online:
+                    if (powerPercent >= 99)
+                    {
+                        this.TopMost = true;
+                        this.FormBorderStyle = FormBorderStyle.None;
+                        this.WindowState = FormWindowState.Maximized;
+                        batteryFull.Text = "Battery FULL!!";
+                    }
+                    else if (powerPercent == 100 && power.BatteryChargeStatus.ToString() == "NoSystemBattery")
+                    {
+                        this.TopMost = false;
+                        this.FormBorderStyle = FormBorderStyle.Sizable;
+                        this.WindowState = FormWindowState.Normal;
+                        batteryFull.Text = "No Battery!!";
+                    }
+                    else 
+                    {
+                        this.FormBorderStyle = FormBorderStyle.Sizable;
+                        this.TopMost = false;
+                        batteryFull.Text = "Charging!!";
+                    }
+                    groupBox1.Text = "Charger Connected";
+                    float secondsRemaining = power.BatteryLifePercent;
+                    if (secondsRemaining >= 0)
+                    {
+                        ChargeRemaining.Text = (secondsRemaining * 100).ToString() + "% available.";
+                    }
+                    else
+                    {
+                        ChargeRemaining.Text = string.Empty;
+                    }
+                    BatteryStatus.Text = power.BatteryChargeStatus.ToString();
+                    break;
+
+                case PowerLineStatus.Offline:
+
+                    if (this.TopMost == true && this.FormBorderStyle == FormBorderStyle.None) {
+                        this.TopMost = false;
+                        this.FormBorderStyle = FormBorderStyle.Sizable;
+                        this.WindowState = FormWindowState.Normal;
+                    }
+
+                        secondsRemaining = power.BatteryLifePercent;
+                        ChargeRemaining.Text = (secondsRemaining * 100).ToString() + "% available.";
+                        batteryFull.Text = "Connect Charger!!";
+                        groupBox1.Text = "Running On Battery";
+                        BatteryStatus.Text = power.BatteryChargeStatus.ToString();
+
+                    break;
+
+                case PowerLineStatus.Unknown:                    
+                        batteryFull.Text = "No Battery available";
+                        groupBox1.Text = "Running On Charger";
+                    break;
+            }
+
+        }
+
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            RefreshStatus();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\run", true);
+
+            if (checkBox1.Checked == true)
+            {
+                rk.SetValue("Battery Notifier", Application.ExecutablePath.ToString());
+            }
+            else {
+                rk.DeleteValue("Battery Notifier", false);                
+            }
+        }
+
+    }
+}
